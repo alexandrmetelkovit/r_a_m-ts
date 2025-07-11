71bcb1523d161580cc6221904edfa37d0ee5e498
alexandrmetelkovit <metel-sanya@mail.ru>
2025-07-11 20:54:26 +0300

    реализованы коммпоненты хедера, футера, компонент загрузки

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
-import "./App.css";
+import { Content } from './components/Content/Content';
+import { Footer } from './components/Footer/Footer';
+import { Header } from './components/Header/Header';
+import { Routes, Route } from 'react-router-dom';
+
+import { Personslist } from './pages/PersonsList';
+import { PersonDetails } from './pages/PersonDetails';
 
 function App() {
-  return <>1</>;
+  return (
+    <>
+      <Header />
+      <Content>
+        <Routes>
+          <Route
+            path='/'
+            element={<Personslist />}
+          />
+          <Route
+            path='/details'
+            element={<PersonDetails />}
+          />
+        </Routes>
+      </Content>
+      <Footer text='Made with love by AlexanderMetelkov' />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+import './Footer.scss';
+
+interface TFooterTextProps {
+  text: string;
+}
+
+export const Footer: React.FC<TFooterTextProps> = ({ text }) => {
+  return (
+    <footer className='footer'>
+      <div className='footer__text'>
+        <p>{text}</p>
+      </div>
+    </footer>
+  );
+};
