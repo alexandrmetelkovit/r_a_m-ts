71829049a7f609acf4a2564631bb821bb00cbef9
alexandrmetelkovit <metel-sanya@mail.ru>
2025-07-18 13:32:00 +0300

    создал текстовое поле для поиска и для редактирования имени персонажа

--- src/pages/PersonsList.tsx
+++ src/pages/PersonsList.tsx
@@ -1,5 +1,7 @@
 // import { Loader } from '../components/Loader/Loader';
+import { useState } from 'react';
 import { Dropdown } from '../components/Dropdown/Dropdown';
+import { Input } from '../components/Input/Input';
 import './PersonsList.scss';
 
 interface DropdownOption {
@@ -15,13 +17,16 @@ const optionsSpecies: DropdownOption[] = [
   { value: 'robot', label: 'Robot' }
 ];
 
-const optionsStatus: DropdownOption[] = [
-  { value: 'alive', label: 'Alive', color: 'green' },
-  { value: 'dead', label: 'Dead', color: 'red' },
-  { value: 'unknown', label: 'Unknown', color: 'orange' }
-];
+// const optionsStatus: DropdownOption[] = [
+//   { value: 'alive', label: 'Alive', color: 'green' },
+//   { value: 'dead', label: 'Dead', color: 'red' },
+//   { value: 'unknown', label: 'Unknown', color: 'orange' }
+// ];
 
 export const Personslist = () => {
+  const [personName, setPersonName] = useState('');
+  const [searchName, setSearchName] = useState('');
+
   return (
     <div className='persons-list container'>
       <div className='persons-list__image'>
@@ -35,15 +40,29 @@ export const Personslist = () => {
       </div>
       {/* <Loader text='Loading characters...' /> */}
       <div className='persons-list__body'>
+        <Input
+          variant='personEdit'
+          value={personName}
+          placeholder='Rick Sanchez'
+          onChange={(e) => setPersonName(e.target.value)}
+        />
+
+        <Input
+          variant='default'
+          value={searchName}
+          onChange={(e) => setSearchName(e.target.value)}
+          placeholder='Filter by name...'
+        />
+
         <Dropdown
           selectTitle='Species'
           options={optionsSpecies}
         />
 
-        <Dropdown
+        {/* <Dropdown
           options={optionsStatus}
           variant='small'
-        />
+        /> */}
       </div>
     </div>
   );
