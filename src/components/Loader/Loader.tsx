ca1dc3f88e32c3aadd0e67bc817e64a37b893fa7
alexandrmetelkovit <metel-sanya@mail.ru>
2025-08-22 15:00:12 +0300

    fix: исправил загрузку картинок для лоадеров

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,4 +1,6 @@
 import './Loader.scss';
+import loadingSmall from '../assets/images/Loading-small.png';
+import loadingBig from '../assets/images/Loading-big.png';
 
 type LoaderVariant = 'default' | 'small';
 
@@ -14,9 +16,7 @@ export const Loader = ({ variant, text }: ILoaderTextProps) => {
     <div className='loader'>
       <img
         className='loader__image'
-        src={
-          isSmall ? '/src/assets/images/Loading-small.png' : '/src/assets/images/Loading-big.png'
-        }
+        src={isSmall ? loadingSmall : loadingBig}
         alt='Индикатор загрузки'
         width={isSmall ? 103 : 475}
         height={isSmall ? 101 : 465}
